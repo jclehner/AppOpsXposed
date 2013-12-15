fd11235205c91898846d1859c6229d9b7129732f
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2013-12-15 17:32:20 +0100

    Refactoring

--- src/at/jclehner/appopsxposed/AppOpsEnabler.java
+++ src/at/jclehner/appopsxposed/AppOpsEnabler.java
@@ -23,30 +23,53 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import java.util.List;
 
 import android.content.res.XModuleResources;
+import android.content.res.XResources;
 import android.os.Build;
 import android.preference.PreferenceActivity.Header;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class AppOpsEnabler implements IXposedHookLoadPackage
+public class AppOpsEnabler implements IXposedHookZygoteInit, IXposedHookLoadPackage
 {
-	private static final String APP_OPS_FRAGMENT = "com.android.settings.applications.AppOpsSummary";
+	static final String MODULE_PACKAGE = AppOpsEnabler.class.getPackage().getName();
+	static final String SETTINGS_APK_PACKAGE = "com.android.settings";
+
+	static final String APP_OPS_FRAGMENT = "com.android.settings.applications.AppOpsSummary";
+	static final String APP_OPS_DETAILS_FRAGMENT = "com.android.settings.applications.AppOpsDetails";
+
+	private static final String[] VALID_FRAGMENTS = {
+		APP_OPS_FRAGMENT, APP_OPS_DETAILS_FRAGMENT
+	};
+
+	//private String mModPath = null;
+	private XModuleResources mModRes = null;
+	private XModuleResources mSettingsRes = null;
+	private XSharedPreferences mPrefs = null;
+
+	@Override
+	public void initZygote(StartupParam startupParam) throws Throwable
+	{
+		log("initZygote: modulePath=" + startupParam.modulePath);
+
+		mModRes = XModuleResources.createInstance(startupParam.modulePath, null);
+		mPrefs = new XSharedPreferences(MODULE_PACKAGE);
+	}
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable
 	{
-		if(!lpparam.packageName.equals("com.android.settings"))
+		if(!lpparam.packageName.equals(SETTINGS_APK_PACKAGE))
 			return;
 
-		final XModuleResources settingsRes = XModuleResources.createInstance(lpparam.appInfo.sourceDir, null);
-		final int personalHeaderId = settingsRes.getIdentifier("id/personal_section", null, lpparam.appInfo.packageName);
-		final int appOpsIcon = settingsRes.getIdentifier("drawable/ic_settings_applications", null, lpparam.appInfo.packageName);
-		final int appOpsTitleId = settingsRes.getIdentifier("string/app_ops_setting", null, lpparam.appInfo.packageName);
-
-		final String appOpsTitle = appOpsTitleId != 0 ? settingsRes.getString(appOpsTitleId) : "App ops";
+		mSettingsRes = XModuleResources.createInstance(lpparam.appInfo.sourceDir, null);
 
 		findAndHookMethod("com.android.settings.Settings", lpparam.classLoader,
 				"onBuildHeaders", List.class, new XC_MethodHook() {
@@ -56,31 +79,7 @@ public class AppOpsEnabler implements IXposedHookLoadPackage
 				protected void afterHookedMethod(MethodHookParam param) throws Throwable
 				{
 					final List<Header> headers = (List<Header>) param.args[0];
-					if(headers == null || headers.isEmpty())
-					{
-						XposedBridge.log("No headers available; not adding AppOps");
-						return;
-					}
-
-					final Header appOpsHeader = new Header();
-					appOpsHeader.fragment = APP_OPS_FRAGMENT;
-					appOpsHeader.title = appOpsTitle;
-					appOpsHeader.id = R.id.app_ops_settings;
-					appOpsHeader.iconRes = appOpsIcon;
-
-					for(int i = 0; i != headers.size(); ++i)
-					{
-						if(headers.get(i).id == personalHeaderId)
-						{
-							headers.add(i + 1, appOpsHeader);
-							return;
-						}
-					}
-
-					// If we reach this point, personalHeaderId was not found, so we
-					// just put the AppOps header last
-
-					headers.add(appOpsHeader);
+					addAppOpsHeader(headers);
 				}
 		});
 
@@ -90,12 +89,19 @@ public class AppOpsEnabler implements IXposedHookLoadPackage
 					"isValidFragment", String.class, new XC_MethodHook() {
 
 					@Override
-					protected void afterHookedMethod(MethodHookParam param) throws Throwable
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable
 					{
-						if(!((Boolean) param.getResult()) && param.args[0].equals(APP_OPS_FRAGMENT))
-							param.setResult(true);
+						for(String name : VALID_FRAGMENTS)
+						{
+							if(name.equals(param.args[0]))
+							{
+								param.setResult(true);
+								return;
+							}
+						}
 					}
 			});
+
 		}
 		catch(NoSuchMethodError e)
 		{
@@ -105,5 +111,95 @@ public class AppOpsEnabler implements IXposedHookLoadPackage
 			if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
 				throw e;
 		}
+
+		if(mPrefs.getBoolean("intercept_loadHeadersFromResource", false))
+		{
+			log("Intercepting loadHeadersFromResource");
+
+			final int headersResId = mSettingsRes.getIdentifier("xml/settings_headers", null, SETTINGS_APK_PACKAGE);
+
+			findAndHookMethodRecursive("com.android.settings.Settings", lpparam.classLoader,
+					"loadHeadersFromResource", int.class, List.class, new XC_MethodHook() {
+
+					@SuppressWarnings("unchecked")
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable
+					{
+						final int resIdArg = (Integer) param.args[0];
+						if(resIdArg == headersResId)
+							addAppOpsHeader((List<Header>) param.args[1]);
+					}
+			});
+		}
+	}
+
+	private void addAppOpsHeader(List<Header> headers)
+	{
+		if(headers == null || headers.isEmpty())
+		{
+			log("addAppOpsHeader: list is empty or null");
+			return;
+		}
+
+		final int personalHeaderId = mSettingsRes.getIdentifier("id/personal_section", null, SETTINGS_APK_PACKAGE);
+		final int appOpsIcon = mSettingsRes.getIdentifier("drawable/ic_settings_applications", null, SETTINGS_APK_PACKAGE);
+		final int appOpsTitleId = mSettingsRes.getIdentifier("string/app_ops_setting", null, SETTINGS_APK_PACKAGE);
+		final String appOpsTitle = appOpsTitleId != 0 ? mSettingsRes.getString(appOpsTitleId) : mModRes.getString(R.string.app_ops_setting);
+
+		final Header appOpsHeader = new Header();
+		appOpsHeader.fragment = APP_OPS_FRAGMENT;
+		appOpsHeader.title = appOpsTitle;
+		appOpsHeader.id = R.id.app_ops_settings;
+		appOpsHeader.iconRes = appOpsIcon;
+
+		int personalHeaderIndex = -1;
+
+		for(int i = 0; i != headers.size(); ++i)
+		{
+			if(headers.get(i).id == personalHeaderId)
+				personalHeaderIndex = i;
+			else if(headers.get(i).id == R.id.app_ops_settings)
+			{
+				log("addAppOpsHeader: header was already added");
+				return;
+			}
+		}
+
+		if(personalHeaderIndex != -1)
+			headers.add(personalHeaderIndex + 1, appOpsHeader);
+		else
+			headers.add(appOpsHeader);
+	}
+
+	private static void findAndHookMethodRecursive(String className, ClassLoader classLoader,
+			String methodName, Object... parameterTypesAndCallback)
+	{
+		try
+		{
+			findAndHookMethod(className, classLoader, methodName, parameterTypesAndCallback);
+		}
+		catch(NoSuchMethodError e)
+		{
+			final Class<?> superClass;
+			try
+			{
+				superClass = classLoader.loadClass(className).getSuperclass();
+			}
+			catch(ClassNotFoundException e1)
+			{
+				throw new IllegalStateException(e1);
+			}
+
+			log("findAndHookMethodRecursive: trying " + superClass);
+			findAndHookMethodRecursive(superClass.getName(), classLoader, methodName, parameterTypesAndCallback);
+		}
+	}
+
+	private static void log(String message)
+	{
+		if(!BuildConfig.DEBUG)
+			return;
+
+		XposedBridge.log("AppOpsXposed: " + message);
 	}
 }
