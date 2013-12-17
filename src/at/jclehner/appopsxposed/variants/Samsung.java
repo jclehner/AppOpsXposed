50411805efea69031a2415cec5f60df3219bf45e
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2013-12-17 22:06:21 +0100

    Add some experimental features that may fix issues on Samsung phones

--- src/at/jclehner/appopsxposed/variants/Samsung.java
+++ src/at/jclehner/appopsxposed/variants/Samsung.java
@@ -1,6 +1,7 @@
 package at.jclehner.appopsxposed.variants;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 import static at.jclehner.appopsxposed.Util.debug;
 import static at.jclehner.appopsxposed.Util.log;
@@ -15,56 +16,124 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class Samsung extends ApkVariant
 {
+	// No idea why they used UpperCamelCase for a private member...
+
+	private static final String[][] TAB_INFOS = {
+		{ "SettingsInGeneralTab", "general_headers" },
+		{ "SettingsInMoreTab", "more_headers" }
+	};
+
+	private boolean mIsComplete = false;
+
 	@Override
 	public String manufacturer() {
 		return "Samsung";
 	}
 
 	@Override
-	public boolean blocksStockVariant() {
-		return true;
+	public boolean isComplete() {
+		return mIsComplete;
 	}
 
 	@Override
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
 	{
-		final Class<?> settingsClazz = lpparam.classLoader.loadClass("com.android.settings.Settings");
+		String xmlResName = null;
 
-		XposedBridge.hookAllConstructors(settingsClazz, new XC_MethodHook() {
+		for(String[] tabInfo : TAB_INFOS)
+		{
+			if(hookConstuctorAddFragmentNameToTab(lpparam, tabInfo[0]))
+			{
+				xmlResName = tabInfo[1];
+				break;
+			}
+		}
+
+		if(xmlResName == null)
+		{
+			log("No matching tab-info found; defaulting to general_headers");
+			xmlResName = "general_headers";
+		}
+		else
+			debug("Using xmlResName=" + xmlResName);
+
+		final int xmlResId = Util.getSettingsIdentifier("xml/" + xmlResName);
+		if(xmlResId == 0)
+		{
+			log("xml/" + xmlResId + " not found in resources");
+			return;
+		}
+
+		hookOnCreate(lpparam, xmlResId);
+		hookIsValidFragment(lpparam);
+
+		mIsComplete = true;
+	}
+
+	public static boolean hookConstuctorAddFragmentNameToTab(LoadPackageParam lpparam, final String tabsFieldName)
+	{
+		try
+		{
+			final Class<?> settingsClazz = lpparam.classLoader.loadClass("com.android.settings.Settings");
+			final Field f = XposedHelpers.findField(settingsClazz, tabsFieldName);
+			if(f.getType() != String[].class)
+			{
+				log("Warning: field exists, but not a string array: " + tabsFieldName);
+				return true;
+			}
+
+			XposedBridge.hookAllConstructors(settingsClazz, new XC_MethodHook() {
 					@Override
 					protected void afterHookedMethod(MethodHookParam param) throws Throwable
 					{
-						// No idea why they used UpperCamelCase for a private member...
-						final Field f = XposedHelpers.findField(settingsClazz, "SettingsInGeneralTab");
+
+						final Field f = XposedHelpers.findField(settingsClazz, tabsFieldName);
 						final String[] settingsInMoreTab = (String[]) f.get(param.thisObject);
 
 						f.set(param.thisObject, Util.appendToStringArray(settingsInMoreTab, "AppOpsSummary"));
-						debug("SettingsInGeneralTab=" + f.get(param.thisObject));
+						debug(tabsFieldName + "=" + f.get(param.thisObject));
 					}
-		});
+			});
 
-		XposedHelpers.findAndHookMethod(settingsClazz, "onCreate",
-				Bundle.class, new XC_MethodHook() {
-					@Override
-					protected void afterHookedMethod(MethodHookParam param) throws Throwable
-					{
+			return true;
+		}
+		catch(Throwable t)
+		{
+			log("No " + tabsFieldName + " field?");
+			debug(t);
+			return false;
+		}
+	}
 
-						try
+	public static boolean hookOnCreate(final LoadPackageParam lpparam, final int headersXmlResId)
+	{
+		try
+		{
+			final Class<?> settingsClazz = lpparam.classLoader.loadClass("com.android.settings.Settings");
+			final Field isTabStyleField = XposedHelpers.findField(settingsClazz, "mPhoneTabStyle");
+
+			XposedHelpers.findAndHookMethod(settingsClazz, "onCreate",
+					Bundle.class, new XC_MethodHook() {
+						@Override
+						protected void afterHookedMethod(MethodHookParam param) throws Throwable
 						{
-							final boolean isTabStyle = XposedHelpers.getBooleanField(param.thisObject, "mPhoneTabStyle");
-							debug("Samsung: isTabStyle=" + isTabStyle);
-
-							final int xmlResId = Util.getSettingsIdentifier(isTabStyle ? "xml/general_headers" : "xml/settings_headers");
-							StockAndroid.hookLoadHeadersFromResource(lpparam, xmlResId);
+							final boolean isTabStyle = isTabStyleField.getBoolean(param.thisObject);
+							debug("isTabStyle=" + isTabStyle);
+							StockAndroid.hookLoadHeadersFromResource(lpparam, headersXmlResId);
 						}
-						catch(NoSuchFieldError e)
-						{
-							log("Samsung: isTabStyle not found; assuming list style!");
-							StockAndroid.hookOnBuildHeaders(lpparam);
-						}
-					}
-		});
+			});
 
-		StockAndroid.hookIsValidFragment(lpparam);
+			return true;
+		}
+		catch(NoSuchFieldError e)
+		{
+			log("mPhoneTabStyle not found?; trying xml/settings_headers");
+			return false;
+		}
+		catch(Throwable t)
+		{
+			log(t);
+			return false;
+		}
 	}
 }
