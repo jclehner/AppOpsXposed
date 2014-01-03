37ac0d183359d3ab401dfea6451e229f21c268e9
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2014-01-03 16:50:22 +0100

    Add Sony-specific Intent to launcher; disabled for now!

--- src/at/jclehner/appopsxposed/LauncherActivity.java
+++ src/at/jclehner/appopsxposed/LauncherActivity.java
@@ -1,10 +1,11 @@
 package at.jclehner.appopsxposed;
 
 import android.app.Activity;
-import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
+import at.jclehner.appopsxposed.variants.Sony;
 
 public class LauncherActivity extends Activity
 {
@@ -13,15 +14,22 @@ public class LauncherActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 
-		final Intent intent = new Intent("android.settings.SETTINGS");
-		intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, AppOpsXposed.APP_OPS_FRAGMENT);
+		final Intent intent = new Intent();
+
+		if(false)
+		{
+			Log.i("AppOpsXposed", "Using Sony-specific Intent");
+			intent.setClassName(AppOpsXposed.SETTINGS_PACKAGE, Sony.SLAVE_ACTIVITY_NAME);
+			intent.putExtra(Sony.EXTRA_LAUNCH_APP_OPS, true);
+		}
+		else
+		{
+			Log.i("AppOpsXposed", "Using standard Intent");
+			intent.setAction("android.settings.SETTINGS");
+			intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, AppOpsXposed.APP_OPS_FRAGMENT);
+		}
+
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-
-		/*TaskStackBuilder tsb = TaskStackBuilder.create(this);
-		tsb.addNextIntent(new Intent("android.settings.SETTINGS"));
-		tsb.addNextIntent(intent);
-
-		tsb.startActivities();*/
 
 		startActivity(intent);
 		finish();
