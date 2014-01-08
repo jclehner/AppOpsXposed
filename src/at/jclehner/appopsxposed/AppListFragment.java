ea8519cd8a60b5d4a53a338c8f15fde3f98de5b5
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2014-01-08 19:44:58 +0100

    Boost scrolling performance

--- src/at/jclehner/appopsxposed/AppListFragment.java
+++ src/at/jclehner/appopsxposed/AppListFragment.java
@@ -1,10 +1,12 @@
 package at.jclehner.appopsxposed;
 
 import java.text.Collator;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import android.app.AppOpsManager;
 import android.app.ListFragment;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.AsyncTaskLoader;
@@ -12,7 +14,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.util.Log;
@@ -24,7 +29,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class AppListFragment extends ListFragment implements LoaderCallbacks<List<ApplicationInfo>>
+public class AppListFragment extends ListFragment implements LoaderCallbacks<List<PackageInfo>>
 {
 	private static final String TAG = "AppListFragment";
 
@@ -34,13 +39,13 @@ public class AppListFragment extends ListFragment implements LoaderCallbacks<Lis
 	class AppListAdapter extends BaseAdapter
 	{
 		private final PackageManager mPm;
-		private List<ApplicationInfo> mList;
+		private List<PackageInfo> mList;
 
 		public AppListAdapter(Context context) {
 			mPm = context.getPackageManager();
 		}
 
-		public void setData(List<ApplicationInfo> list)
+		public void setData(List<PackageInfo> list)
 		{
 			mList = list;
 			notifyDataSetChanged();
@@ -52,7 +57,7 @@ public class AppListFragment extends ListFragment implements LoaderCallbacks<Lis
 		}
 
 		@Override
-		public ApplicationInfo getItem(int position) {
+		public PackageInfo getItem(int position) {
 			return mList.get(position);
 		}
 
@@ -64,65 +69,127 @@ public class AppListFragment extends ListFragment implements LoaderCallbacks<Lis
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent)
 		{
-			final View v;
+			final ViewHolder holder;
+			final ApplicationInfo appInfo = getItem(position).applicationInfo;
+
 			if(convertView == null)
-				v = mInflater.inflate(R.layout.app_list_item, parent, false);
+			{
+				convertView = mInflater.inflate(R.layout.app_list_item, parent, false);
+
+				holder = new ViewHolder();
+				holder.appIcon = (ImageView) convertView.findViewById(R.id.app_icon);
+				holder.appPackage = (TextView) convertView.findViewById(R.id.app_package);
+				holder.appName = (TextView) convertView.findViewById(R.id.app_name);
+
+				convertView.setTag(holder);
+			}
 			else
-				v = convertView;
+				holder = (ViewHolder) convertView.getTag();
 
-			final ApplicationInfo info = getItem(position);
-			v.setTag(info.packageName);
+			holder.appIcon.setImageDrawable(null);
+			holder.appName.setText(appInfo.packageName);
+			//holder.appPackage.setText(appInfo.packageName);
+			//holder.appPackage.setVisibility(View.VISIBLE);
 
-			final CharSequence label = info.loadLabel(mPm);
-			if(label.toString().equals(info.packageName))
-				v.findViewById(R.id.app_package).setVisibility(View.GONE);
+			if(true)
+			{
+				if(holder.task != null)
+					holder.task.cancel(true);
+
+				holder.task = new AsyncTask<Void, Void, Object[]>() {
+					@Override
+					protected Object[] doInBackground(Void... params)
+					{
+						final Object[] result = new Object[2];
+						result[0] = appInfo.loadIcon(mPm);
+						result[1] = appInfo.loadLabel(mPm);
+
+						return result;
+					}
+
+					@Override
+					protected void onPostExecute(Object[] result)
+					{
+						holder.appIcon.setImageDrawable((Drawable) result[0]);
+						holder.appName.setText((CharSequence) result[1]);
+
+						if(!appInfo.packageName.equals(result[1].toString()))
+						{
+							holder.appPackage.setText(appInfo.packageName);
+							holder.appPackage.setVisibility(View.VISIBLE);
+						}
+						else
+							holder.appPackage.setVisibility(View.GONE);
+					}
+
+				}.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+				holder.packageName = appInfo.packageName;
+			}
 			else
-				v.findViewById(R.id.app_package).setVisibility(View.VISIBLE);
+			{
+				holder.appIcon.setImageDrawable(appInfo.loadIcon(mPm));
+				holder.appName.setText(appInfo.loadLabel(mPm));
+			}
 
-			((ImageView) v.findViewById(R.id.app_icon)).setImageDrawable(info.loadIcon(mPm));
-			((TextView) v.findViewById(R.id.app_name)).setText(label);
-			((TextView) v.findViewById(R.id.app_package)).setText(info.packageName);
-
-			return v;
+			return convertView;
 		}
 	}
 
-	static class AppInfoComparator implements Comparator<ApplicationInfo>
+	static class ViewHolder
+	{
+		String packageName;
+		AsyncTask<Void, Void, Object[]> task;
+
+		ImageView appIcon;
+		TextView appName;
+		TextView appPackage;
+	}
+
+	static class PkgInfoComparator implements Comparator<PackageInfo>
 	{
 		private static Collator sCollator = Collator.getInstance();
 		private final PackageManager mPm;
 
-		public AppInfoComparator(PackageManager pm) {
+		public PkgInfoComparator(PackageManager pm) {
 			mPm = pm;
 		}
 
 		@Override
-		public int compare(ApplicationInfo lhs, ApplicationInfo rhs) {
-			return sCollator.compare(lhs.loadLabel(mPm), rhs.loadLabel(mPm));
+		public int compare(PackageInfo lhs, PackageInfo rhs)
+		{
+			return sCollator.compare(lhs.applicationInfo.loadLabel(mPm),
+					rhs.applicationInfo.loadLabel(mPm));
 		}
 	}
 
-	static class AppListLoader extends AsyncTaskLoader<List<ApplicationInfo>>
+	static class AppListLoader extends AsyncTaskLoader<List<PackageInfo>>
 	{
-		private List<ApplicationInfo> mData;
-		private PackageManager mPm;
+		private static String[] sOpPerms = (String[])
+				Util.getStaticObjectField(AppOpsManager.class, "sOpPerms");
+
+		private final PackageManager mPm;
+		private List<PackageInfo> mData;
 
 		public AppListLoader(Context context)
 		{
 			super(context);
 			mPm = context.getPackageManager();
+			Log.d(TAG, "sOpPerms=" + Arrays.toString(sOpPerms));
 		}
 
 		@Override
-		public List<ApplicationInfo> loadInBackground()
+		public List<PackageInfo> loadInBackground()
 		{
-			List<ApplicationInfo> data = mPm.getInstalledApplications(0);
-			Collections.sort(data, new AppInfoComparator(mPm));
+			List<PackageInfo> data = mPm.getInstalledPackages(PackageManager.GET_PERMISSIONS);
+			if(sOpPerms != null)
+				removeAppsWithoutOps(data);
+			Collections.sort(data, new PkgInfoComparator(mPm));
 			return data;
 		}
 
 		@Override
-		public void deliverResult(List<ApplicationInfo> data)
+		public void deliverResult(List<PackageInfo> data)
 		{
 			mData = data;
 
@@ -154,15 +221,54 @@ public class AppListFragment extends ListFragment implements LoaderCallbacks<Lis
 			onStopLoading();
 			mData = null;
 		}
+
+		private void removeAppsWithoutOps(List<PackageInfo> data)
+		{
+			Log.i(TAG, "removeAppsWithoutOps: ");
+
+			for(int i = 0; i != data.size(); ++i)
+			{
+				if(!hasAppOps(data.get(i)))
+				{
+					data.remove(i);
+					--i;
+				}
+			}
+		}
+
+		private boolean hasAppOps(PackageInfo info)
+		{
+			if(info.requestedPermissions != null)
+			{
+				for(String permName : info.requestedPermissions)
+				{
+					if(isAppOpsPermission(permName))
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+		private boolean isAppOpsPermission(String permName)
+		{
+			for(String opPerm : sOpPerms)
+			{
+				if(opPerm != null && permName.equals(opPerm))
+					return true;
+			}
+
+			return false;
+		}
 	}
 
 	@Override
-	public Loader<List<ApplicationInfo>> onCreateLoader(int id, Bundle args) {
+	public Loader<List<PackageInfo>> onCreateLoader(int id, Bundle args) {
 		return new AppListLoader(getActivity());
 	}
 
 	@Override
-	public void onLoadFinished(Loader<List<ApplicationInfo>> loader, List<ApplicationInfo> data)
+	public void onLoadFinished(Loader<List<PackageInfo>> loader, List<PackageInfo> data)
 	{
 		mAdapter.setData(data);
 
@@ -173,7 +279,7 @@ public class AppListFragment extends ListFragment implements LoaderCallbacks<Lis
 	}
 
 	@Override
-	public void onLoaderReset(Loader<List<ApplicationInfo>> data) {
+	public void onLoaderReset(Loader<List<PackageInfo>> data) {
 		mAdapter.setData(null);
 	}
 
@@ -196,7 +302,7 @@ public class AppListFragment extends ListFragment implements LoaderCallbacks<Lis
 	public void onListItemClick(ListView l, View v, int position, long id)
 	{
 		final Bundle args = new Bundle();
-		args.putString("package", (String) v.getTag());
+		args.putString("package", ((ViewHolder) v.getTag()).packageName);
 
 		final Intent intent = new Intent("android.settings.SETTINGS");
 		intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, AppOpsXposed.APP_OPS_DETAILS_FRAGMENT);
