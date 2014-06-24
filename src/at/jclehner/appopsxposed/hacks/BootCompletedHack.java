jclehner/AppOpsXposed

commit c6ea69058521b8fc161b5acda25694978ea7f83b

Remove Hack.handleLoadAnyPackage

src/at/jclehner/appopsxposed/hacks/BootCompletedHack.java
@@ -34,15 +34,15 @@ public class BootCompletedHack extends Hack
 	private static final int OP_BOOT_COMPLETED = OP_POST_NOTIFICATION;
 
 	@Override
-	public void handleLoadAnyPackage(LoadPackageParam lpparam) throws Throwable {
-		patchFrameworkPart(lpparam.classLoader);
-	}
-
-	@Override
 	public void handleLoadSettingsPackage(LoadPackageParam lpparam) throws Throwable
 	{
 		injectLabelAndSummary(lpparam);
 		addBootupTemplate(lpparam);
+	}
+
+	@Override
+	protected void handleLoadFrameworkPackage(LoadPackageParam lpparam) throws Throwable {
+		patchFrameworkPart(lpparam.classLoader);
 	}
 
 	private void patchFrameworkPart(ClassLoader classLoader)

src/at/jclehner/appopsxposed/Hack.java
@@ -26,8 +26,6 @@ public class Hack implements IXposedHookLoadPackage, IXposedHookZygoteInit
 			handleLoadFrameworkPackage(lpparam);
 		else if(AppOpsXposed.SETTINGS_PACKAGE.equals(lpparam.packageName))
 			handleLoadSettingsPackage(lpparam);
-
-		handleLoadAnyPackage(lpparam);
 	}
 
 	protected void handleLoadFrameworkPackage(LoadPackageParam lpparam) throws Throwable {
@@ -35,10 +33,6 @@ public class Hack implements IXposedHookLoadPackage, IXposedHookZygoteInit
 	}
 
 	protected void handleLoadSettingsPackage(LoadPackageParam lpparam) throws Throwable {
-
-	}
-
-	protected void handleLoadAnyPackage(LoadPackageParam lpparam) throws Throwable {
 
 	}
 

src/at/jclehner/appopsxposed/SettingsActivity.java
@@ -123,7 +123,7 @@ public class SettingsActivity extends Activity
 				@Override
 				public boolean onPreferenceClick(Preference preference)
 				{
-					if(true || mPrefs.getBoolean("show_hacks_warning_dialog", true))
+					if(mPrefs.getBoolean("show_hacks_warning_dialog", true))
 						showHacksWarningDialog(preference);
 
 					return true;

commit 63efbc6c94890343cfdd49eb3ac686f177e05560

Fix issue introduced in c6ea690

src/at/jclehner/appopsxposed/hacks/BootCompletedHack.java
@@ -76,6 +76,7 @@ public class BootCompletedHack extends Hack
 	@Override
 	public void handleLoadSettingsPackage(LoadPackageParam lpparam) throws Throwable
 	{
+		patchFrameworkPart(lpparam.classLoader);
 		injectLabelAndSummary(lpparam);
 		addBootupTemplate(lpparam);
 	}
@@ -306,7 +307,6 @@ public class BootCompletedHack extends Hack
 			final Object[][] adapterReturnValueInfos = {
 					{ "getPageTitle", Util.getModString(R.string.app_ops_categories_bootup) },
 					{ "getItem", bootupCategoryFragment }
-
 			};
 
 			for(final Object[] adapterReturnValueInfo : adapterReturnValueInfos)
@@ -415,7 +415,7 @@ public class BootCompletedHack extends Hack
 
 	static class AppOpsManagerReturnValues
 	{
-		//static final int opToSwitch = OP_BOOT_COMPLETED;
+		static final int opToSwitch = OP_BOOT_COMPLETED;
 		static final String opToName = "BOOT_COMPLETED";
 		static final String opToPermission = Manifest.permission.RECEIVE_BOOT_COMPLETED;
 		static final int opToDefaultMode = AppOpsManager.MODE_ALLOWED;