a65e47ff4732344cbf308156b7992da04bc1b9ed
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2015-01-06 19:57:13 +0100

    Show dialog if AppOps permissions were not granted

--- src/at/jclehner/appopsxposed/AppOpsActivity.java
+++ src/at/jclehner/appopsxposed/AppOpsActivity.java
@@ -1,7 +1,11 @@
 package at.jclehner.appopsxposed;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import at.jclehner.appopsxposed.util.Constants;
 
 import com.android.settings.applications.AppOpsCategory;
 import com.android.settings.applications.AppOpsDetails;
@@ -19,11 +23,35 @@ public class AppOpsActivity extends PreferenceActivity
 		return intent;
 	}
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		checkForAppOpsPermissions();
+	}
+
+	@Override
 	protected boolean isValidFragment(String fragmentName)
 	{
 		return AppOpsSummary.class.getName().equals(fragmentName)
 				|| AppOpsDetails.class.getName().equals(fragmentName)
 				|| AppOpsCategory.class.getName().equals(fragmentName)
 				|| AppListFragment.class.getName().equals(fragmentName);
+	}
+
+	private void checkForAppOpsPermissions()
+	{
+		for(String perm : Constants.APP_OPS_PERMISSIONS)
+		{
+			if(checkCallingOrSelfPermission(perm) != PackageManager.PERMISSION_GRANTED)
+			{
+				final AlertDialog.Builder ab = new AlertDialog.Builder(this);
+				ab.setMessage(getString(R.string.permissions_not_granted,
+						getString(R.string.app_ops_settings),
+						getString(R.string.compatibility_mode_title)));
+				ab.setPositiveButton(android.R.string.ok, null);
+				ab.show();
+			}
+		}
 	}
 }
