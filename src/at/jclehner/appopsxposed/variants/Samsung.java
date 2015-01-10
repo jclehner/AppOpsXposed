8181ec65aed6a2d37bdfb8db2f89e57003b28591
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2015-01-10 11:51:47 +0100

    Potential fix for GridSettings bug

--- src/at/jclehner/appopsxposed/variants/Samsung.java
+++ src/at/jclehner/appopsxposed/variants/Samsung.java
@@ -21,10 +21,17 @@ package at.jclehner.appopsxposed.variants;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceActivity.Header;
 import at.jclehner.appopsxposed.ApkVariant;
 import at.jclehner.appopsxposed.AppOpsXposed;
 import at.jclehner.appopsxposed.util.Res;
 import at.jclehner.appopsxposed.util.Util;
+import at.jclehner.appopsxposed.util.XUtils;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
@@ -33,6 +40,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class Samsung extends ApkVariant
 {
 	public static final String GRID_SETTINGS = "com.android.settings.GridSettings";
+
+	private boolean mIsUsingGridSettings = false;
 
 	@Override
 	protected String manufacturer() {
@@ -73,9 +82,12 @@ public class Samsung extends ApkVariant
 				Res.getSettingsIdentifier("xml/management_headers"),
 				Res.getSettingsIdentifier("xml/grid_settings_headers")
 		};
-		final int manageAppsHeaderId = Res.getSettingsIdentifier("id/application_settings");
 
 		debug("xmlHookResIds=" + Arrays.toString(xmlHookResIds));
+
+		final int manageAppsHeaderId = Res.getSettingsIdentifier("id/application_settings");
+
+		hookLoadHeadersFromResource(lpparam, AppOpsXposed.SETTINGS_MAIN_ACTIVITY, xmlHookResIds, manageAppsHeaderId);
 
 		if(hasGridSettings(lpparam))
 		{
@@ -84,35 +96,27 @@ public class Samsung extends ApkVariant
 			final int gridId = Res.getSettingsIdentifier("bool/settings_grid");
 			debug("bool/settings_grid=" + gridId);
 
-			final boolean dontHookNormalSettings = gridId != 0 && Res.settingsRes.getBoolean(gridId);
+			mIsUsingGridSettings = gridId != 0 && Res.settingsRes.getBoolean(gridId);
 
 			hookIsValidFragment(lpparam, GRID_SETTINGS);
 			hookLoadHeadersFromResource(lpparam, GRID_SETTINGS, xmlHookResIds, manageAppsHeaderId);
-
-			if(dontHookNormalSettings)
-			{
-				debug(GRID_SETTINGS + " is enabled; not hooking " + AppOpsXposed.SETTINGS_MAIN_ACTIVITY);
-				return;
-			}
+			hookGridSettingsHeaderClick(lpparam);
 		}
-
-		hookLoadHeadersFromResource(lpparam, AppOpsXposed.SETTINGS_MAIN_ACTIVITY, xmlHookResIds, manageAppsHeaderId);
 	}
 
-	/*@Override
+	@Override
 	protected Object onCreateAppOpsHeader(Context context, int addAfterHeaderId)
 	{
 		final Header header = (Header) super.onCreateAppOpsHeader(context, addAfterHeaderId);
-		if(addAfterHeaderId == Util.getSettingsIdentifier("xml/grid_settings_headers"))
-			header.iconRes = Util.getSettingsIdentifier("drawable/ic_setting_grid_applicationpermissions");
+		if(mIsUsingGridSettings && header.intent == null)
+		{
+			header.intent = new Intent();
+			header.intent.setClassName(context.getPackageName(), AppOpsXposed.SETTINGS_MAIN_ACTIVITY);
+			header.intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, AppOpsXposed.APP_OPS_FRAGMENT);
+		}
 
 		return header;
-	}*/
-
-	/*@Override
-	protected boolean onMatch(LoadPackageParam lpparam) {
-		return lpparam.appInfo.sourceDir.toLowerCase(Locale.US).endsWith("SecSettings.apk".toLowerCase(Locale.US));
-	}*/
+	}
 
 	@Override
 	protected String[] indicatorClasses() {
@@ -133,7 +137,7 @@ public class Samsung extends ApkVariant
 			final Field f = XposedHelpers.findField(settingsClazz, tabsFieldName);
 			if(f.getType() != String[].class)
 			{
-				log("Field exists, but not a string array: " + tabsFieldName);
+				debug("Field exists, but not a string array: " + tabsFieldName);
 				return false;
 			}
 
@@ -158,6 +162,40 @@ public class Samsung extends ApkVariant
 			debug(t);
 			return false;
 		}
+	}
+
+	private void hookGridSettingsHeaderClick(LoadPackageParam lpparam) throws Throwable
+	{
+		XUtils.findAndHookMethodRecursive(GRID_SETTINGS, lpparam.classLoader,
+				"onHeaderClick", Header.class, int.class, new XC_MethodHook() {
+
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						setRegularSettingsEnabled(param, true);
+					}
+
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						setRegularSettingsEnabled(param, false);
+					}
+
+					private void setRegularSettingsEnabled(MethodHookParam param, boolean enabled)
+					{
+						try
+						{
+							final Context ctx = (Context) param.thisObject;
+							ctx.getPackageManager().setComponentEnabledSetting(
+									new ComponentName(ctx.getPackageName(), AppOpsXposed.SETTINGS_MAIN_ACTIVITY),
+									enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+									: PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+									PackageManager.DONT_KILL_APP);
+						}
+						catch(Throwable t)
+						{
+							debug(t);
+						}
+					}
+		});
 	}
 
 	private boolean hasGridSettings(LoadPackageParam lpparam)
