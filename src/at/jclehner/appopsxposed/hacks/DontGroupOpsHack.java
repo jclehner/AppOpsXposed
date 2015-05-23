5b046ab6f1627525d19a2cf595e084f6262d197c
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2015-05-23 17:35:21 +0200

    Be more verbose

--- src/at/jclehner/appopsxposed/hacks/DontGroupOpsHack.java
+++ src/at/jclehner/appopsxposed/hacks/DontGroupOpsHack.java
@@ -2,13 +2,14 @@ package at.jclehner.appopsxposed.hacks;
 
 import java.lang.reflect.Method;
 
+import android.app.AppOpsManager;
+import android.util.Log;
+import at.jclehner.appopsxposed.Hack;
+import at.jclehner.appopsxposed.util.AppOpsManagerWrapper;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-import android.app.AppOpsManager;
-import at.jclehner.appopsxposed.Hack;
-import at.jclehner.appopsxposed.util.AppOpsManagerWrapper;
 
 public class DontGroupOpsHack extends Hack {
 
@@ -35,7 +36,12 @@ public class DontGroupOpsHack extends Hack {
 					@Override
 					protected void beforeHookedMethod(MethodHookParam param) throws Throwable
 					{
-						param.setResult(XposedBridge.invokeOriginalMethod(opToSwitch, null, param.args));
+						final int opGroup = (int) XposedBridge.invokeOriginalMethod(
+								opToSwitch, null, param.args);
+
+						param.setResult(opGroup);
+						Log.i("AOX", "opToGroup called: op=" + param.args[0] + "; opGroup=" + opGroup);
+
 					}
 
 		});
