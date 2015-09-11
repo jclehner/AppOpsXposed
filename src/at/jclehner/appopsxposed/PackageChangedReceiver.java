f8a2b13e431263b00e8d1b7edb5c9f51d120b28a
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2015-09-11 18:17:35 +0200

    Show notifications on app install

---
+++ src/at/jclehner/appopsxposed/PackageChangedReceiver.java
@@ -0,0 +1,85 @@
+package at.jclehner.appopsxposed;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+
+import at.jclehner.appopsxposed.util.Util;
+
+public class PackageChangedReceiver extends BroadcastReceiver
+{
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+		if(!sp.getBoolean("show_package_notifications", true))
+			return;
+
+		final PackageManager pm = context.getPackageManager();
+		final PackageInfo pi = getPkgInfo(context, intent);
+		if(pi == null || pi.applicationInfo.packageName.equals(context.getPackageName()))
+			return;
+
+		final Notification.Builder nb = new Notification.Builder(context);
+		nb.setLargeIcon(drawableToBitmap(pi.applicationInfo.loadIcon(pm)));
+		nb.setSmallIcon(R.drawable.ic_appops_sense);
+		nb.setContentTitle(context.getString(R.string.package_updated));
+		nb.setContentText(pi.applicationInfo.loadLabel(pm) + " " + pi.versionName);
+		nb.setAutoCancel(true);
+		nb.setPriority(Notification.PRIORITY_LOW);
+		nb.setContentIntent(PendingIntent.getActivity(context, 0,
+				Util.getCompatibilityModeIntent(pi.packageName), PendingIntent.FLAG_CANCEL_CURRENT));
+
+		final NotificationManager nm =
+				(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		nm.notify(0, nb.build());
+	}
+
+	private PackageInfo getPkgInfo(Context context, Intent intent)
+	{
+		final Uri data = intent.getData();
+		if(data != null)
+		{
+			try
+			{
+				return context.getPackageManager().getPackageInfo(
+						data.getEncodedSchemeSpecificPart(), 0);
+			}
+			catch(PackageManager.NameNotFoundException e)
+			{
+				// ignore
+			}
+		}
+		return null;
+	}
+
+	private static Bitmap drawableToBitmap(Drawable drawable)
+	{
+		if(drawable instanceof BitmapDrawable)
+			return ((BitmapDrawable) drawable).getBitmap();
+
+		int width = drawable.getIntrinsicWidth();
+		width = width > 0 ? width : 1;
+		int height = drawable.getIntrinsicHeight();
+		height = height > 0 ? height : 1;
+
+		final Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		final Canvas canvas = new Canvas(bitmap);
+		drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+		drawable.draw(canvas);
+
+		return bitmap;
+	}
+}
