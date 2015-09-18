ed730a89ca23e12eed0e07f12572a8bb1b19aa53
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2015-09-18 20:33:15 +0200

    Support AOSP Lollipop settings

--- src/at/jclehner/appopsxposed/variants/AOSP.java
+++ src/at/jclehner/appopsxposed/variants/AOSP.java
@@ -24,8 +24,10 @@ import android.content.Context;
 import android.os.Build;
 import android.preference.PreferenceActivity.Header;
 import at.jclehner.appopsxposed.ApkVariant;
+import at.jclehner.appopsxposed.R;
 import at.jclehner.appopsxposed.util.Constants;
 import at.jclehner.appopsxposed.util.Res;
+import at.jclehner.appopsxposed.util.Util;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -38,7 +40,9 @@ public class AOSP extends ApkVariant
 		addAppOpsToAppInfo(lpparam);
 
 		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
-			addAppOpsHeaderPreLollipop(lpparam);
+			addAppOpsHeader(lpparam);
+		else
+			addAppOpsDashboardTile(lpparam);
 	}
 
 	@Override
@@ -46,7 +50,7 @@ public class AOSP extends ApkVariant
 		return Constants.ICON_SHIELD_WHITE;
 	}
 
-	private void addAppOpsHeaderPreLollipop(LoadPackageParam lpparam)
+	private void addAppOpsHeader(LoadPackageParam lpparam)
 	{
 		final int settingsHeadersId = Res.getSettingsIdentifier("xml/settings_headers");
 		final int personalSectionId = Res.getSettingsIdentifier("id/personal_section");
@@ -78,5 +82,71 @@ public class AOSP extends ApkVariant
 						addAppOpsHeader((List<Header>) param.args[0], personalSectionId, (Context) param.thisObject);
 					}
 		});
+	}
+
+	private void addAppOpsDashboardTile(LoadPackageParam lpparam) throws Throwable
+	{
+		final Class<?> tileClazz = lpparam.classLoader.loadClass(
+				"com.android.settings.dashboard.DashboardTile");
+
+		final int categoriesXmlResId = Res.getSettingsIdentifier("xml/dashboard_categories");
+		final int personalSectionResId = Res.getSettingsIdentifier("id/personal_section");
+
+		XposedHelpers.findAndHookMethod("com.android.settings.SettingsActivity",
+				lpparam.classLoader, "loadCategoriesFromResource", int.class, List.class,
+				new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable
+					{
+						if(categoriesXmlResId != (int) param.args[0])
+							return;
+
+						for(Object category : (List<?>) param.args[1])
+						{
+							if(personalSectionResId == XposedHelpers.getLongField(category, "id"))
+							{
+								final Object tile = tileClazz.newInstance();
+								XposedHelpers.setLongField(tile, "id", R.id.app_ops_settings);
+								XposedHelpers.setIntField(tile, "iconRes", getAppOpsHeaderIcon());
+								XposedHelpers.setObjectField(tile, "title", getAppOpsTitle());
+								XposedHelpers.setObjectField(tile, "intent", Util.createAppOpsIntent(null));
+
+								// Try three approaches: addTile(int, DashboardTile) may not be
+								// available, so we try to add it to the list managed by this
+								// DashboardCategory. If that fails, addTile() should work, but
+								// adds our entry at the end of the list.
+
+								for(int i = 0; i != 3; ++i)
+								{
+									try
+									{
+										if(i == 0)
+										{
+											XposedHelpers.callMethod(category, "addTile",
+													new Class<?>[] { int.class, tileClazz },
+													0, tile);
+										}
+										else if(i == 1)
+										{
+											((List) XposedHelpers.getObjectField(category, "tiles"))
+													.add(0, tile);
+										}
+										else
+										{
+											XposedHelpers.callMethod(category, "addTile", tile);
+										}
+									}
+									catch(Throwable t)
+									{
+										debug(t);
+									}
+								}
+
+								return;
+							}
+						}
+					}
+		});
+
 	}
 }
