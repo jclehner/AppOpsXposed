8df8412ea467414e91e028107ffd60b1b8a09de8
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-02-20 15:41:19 +0100

    Add missing labels

--- opstring-label-checker.sh
+++ opstring-label-checker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for x in $(cat res/values/extracted.xml | grep -oP 'app_ops_summaries_\w+'); do 
+for x in $(cat res/values/ops.xml | grep -oP 'app_ops_summaries_\w+'); do
 	op=${x:18}
-   	if ! grep -q app_ops_labels_$op res/values/extracted.xml; then 
-		echo "Missing: app_ops_labels_$op"
+	if ! grep -q app_ops_labels_$op res/values/ops.xml; then
+		echo "<string name=\"app_ops_labels_$op\">@string/app_ops_summaries_$op</string>"
+		#echo "Missing: app_ops_labels_$op"
 	fi
 done
